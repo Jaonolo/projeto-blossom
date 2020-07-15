e4dee5a2b56e71d280151cdbc18696741ec6a979
João Vitor Sanches <Joo Vitor Cezario Sanches>
2020-07-15 14:46:17 -0300

    Commit para teste do site no servidor Netlify

--- src/container/landing/landing.container.js
+++ src/container/landing/landing.container.js
@@ -6,9 +6,8 @@ export default class LandingContainer extends Component{
     render(){
         return(
             <div className="landing">
-                <img src={logo} className="logo" alt="logo"></img>
                 <div className="landing-titulo-holder">
-                    <h1 className="landing-titulo">A new era at software<br></br>development</h1>
+                    <h1 className="landing-titulo">Ensinar Programação,<br></br>reinventado.</h1>
                     {/* <h2 className="landing-comentario">Tecnology at its finest</h2> */}
                 </div>
             </div>
