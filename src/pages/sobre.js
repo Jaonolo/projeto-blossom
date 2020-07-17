3dd8683229bade0f673c8afaf5461245b7417036
João Vitor Sanches <Joo Vitor Cezario Sanches>
2020-07-17 15:26:00 -0300

    DESESPERO

---
+++ src/pages/sobre.js
@@ -0,0 +1,25 @@
+import React from "react"
+import PadrãoTemplate from "../templates/padrão/padrão.template"
+// import "./404.style.scss"
+
+const DownloadPage = () => (
+  <PadrãoTemplate>
+    {/* <div className="download">
+        <div className="download_conteúdo">
+            <h1 className="download_título">Faça o download da<br></br>última versão já!</h1>
+            <a href="https://github.com/Jaonolo/Projeto-Blossom/archive/master.zip">
+                <div className="download_botão">
+                    <h1 className="download_botão_título">Download</h1>
+                </div>
+            </a>
+        </div>
+    </div> */}
+    <div className="sobre_texto">
+        <span className="sobre_título"></span>
+        lorem
+        <span className="sobre_minitítulo"></span>
+    </div>
+  </PadrãoTemplate>
+)
+
+export default DownloadPage
